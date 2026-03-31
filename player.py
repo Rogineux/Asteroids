3d03219bbb5aeb87d6c2eb6445c2a4466c690158
Rogineux <rogineux@gmail.com>
2026-03-31 19:50:18 +0200

    initial commit

---
+++ player.py
@@ -0,0 +1,68 @@
+import pygame
+from circleshape import CircleShape
+from shot import Shot
+from constants import (
+    PLAYER_RADIUS,
+    LINE_WIDTH,
+    PLAYER_TURN_SPEED,
+    PLAYER_SPEED,
+    SHOT_RADIUS,
+    PLAYER_SHOOT_SPEED,
+    PLAYER_SHOOT_COOLDOWN_SECONDS
+)
+
+class Player(CircleShape):
+
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
+        self.shot_cooldown_timer = 0
+
+    # in the Player class
+    def shoot(self):
+        if self.shot_cooldown_timer > 0:
+            return
+
+        self.shot_cooldown_timer = PLAYER_SHOOT_COOLDOWN_SECONDS
+
+        shot = Shot(self.position.x, self.position.y)
+        shot_velocity = pygame.Vector2(0,1)
+        shot_velocity = shot_velocity.rotate(self.rotation)
+        shot_velocity *= PLAYER_SHOOT_SPEED
+        shot.velocity = shot_velocity
+
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+
+    def move(self, dt):
+        unit_vector = pygame.Vector2(0, 1)
+        rotated_vector = unit_vector.rotate(self.rotation)
+        rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
+        self.position += rotated_with_speed_vector
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def draw(self, screen):
+        pygame.draw.polygon(screen, 'white', self.triangle(), LINE_WIDTH)
+
+    def update(self, dt):
+        self.shot_cooldown_timer -= dt
+
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
